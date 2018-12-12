1299bd8a084cf89d149efce7d12a81eccd89d2e1
Griko Nibras <grikomsn@gmail.com>
2018-12-12 13:18:37 +0700

    ✏️ Update footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,17 @@ export default () => (
       <small className='has-text-grey'>
         Copyright &copy; DSC PENS 2018.
         <br />
-        Hosted on <Link href='https://zeit.co/now'>Zeit Now</Link>, source code
-        available on{' '}
+        Hosted on <Link href='https://firebase.google.com'>Firebase</Link>,
+        source code available on{' '}
         <Link href='https://github.com/dsc-pens/website'>GitHub</Link>.
+        <br />
+        <br />
+        <Link href='https://travis-ci.com/dsc-pens/website'>
+          <img
+            src='https://travis-ci.com/dsc-pens/website.svg?branch=master'
+            alt='Travis CI Build Status'
+          />
+        </Link>
       </small>
     </div>
   </footer>
